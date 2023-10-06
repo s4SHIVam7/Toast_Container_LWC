3f2767f3b38ba23779f660bdb02e0c4ad18469b7
s4SHIVam7 <shiv.laidwar@gmail.com>
2023-10-06 22:35:23 +0530

    js file added.

--- force-app/main/default/lwc/toast_Container_Lwc/toast_Container_Lwc.js
+++ force-app/main/default/lwc/toast_Container_Lwc/toast_Container_Lwc.js
@@ -1,4 +1,36 @@
-import { LightningElement, track } from 'lwc';
-export default class Toast_Container_Lwc extends LightningElement {
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+export default class Toast_Container_lwc extends LightningElement {
+    showToast(variant, title, message, url, label) {
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            messageData: [
+                label,
+                {
+                    url: url,
+                    label: label
+                }
+            ],
+            variant: variant,
+        });
+        this.dispatchEvent(toastEvent);
+    }
+
+    showError() {
+        this.showToast('error', 'Error', 'Record {0} created! See it {1}!', 'https://www.salesforce.com/', 'here');
+    }
+
+    showWarning() {
+        this.showToast('warning', 'Warning', 'Record {0} created! See it {1}!', 'https://www.salesforce.com/', 'here');
+    }
+
+    showSuccess() {
+        this.showToast('success', 'Success', 'Record {0} created! See it {1}!', 'https://www.salesforce.com/', 'here');
+    }
+
+    showInfo() {
+        this.showToast('info', 'Info', 'Record {0} created! See it {1}!', 'https://www.salesforce.com/', 'here');
+    }
 }
